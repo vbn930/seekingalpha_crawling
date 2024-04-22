c813a210d76b91e2b413cbcd745daa90f7772e53
vbn930 <vbn930@naver.com>
2024-04-22 15:23:28 +0900

    add: 브라우저 대기시간 설정 및 프로그램 마무리

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ from Manager import DriverManager
 from Utility import LoginModule
 from Utility import Util
 import atexit
+import seekingalpha_crawler
 
 #pyinstaller -n "TREX Crawler ver1.0" --clean --onefile main.py
 
 def main():
-    logger = Util.Logger("Build")
+    logger = Util.Logger("Dev")
     logger.log(log_level="Event", log_msg=f"=TREX Crawler ver1.0=")
-    
+    crawler = seekingalpha_crawler.SeekingAlpha_Crawler(logger)
+    #crawler.start_crawling()
     try:
-        #crawler.start_crawling()
+        crawler.start_crawling()
         return
     except Exception as e:
         logger.log(log_level="Error", log_msg=e)
